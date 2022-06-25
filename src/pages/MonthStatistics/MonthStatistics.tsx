35c53e4eabe33c8c1b8df2b1fa5fbd0fb1a7ea7e
Ilia Filimonov <9991571155i@gmail.com>
2022-06-25 22:44:56 +0300

    added pages

---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./MonthStatistics.module.scss";
+
+interface Props {}
+
+export const MonthStatistics = (props: Props) => {
+  return <h2 className="{s.title}">Month Statistics</h2>;
+};
