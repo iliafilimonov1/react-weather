a316ca0d1c3009a505169bbb1ceb083d038b9994
Ilia Filimonov <9991571155i@gmail.com>
2022-06-25 22:45:19 +0300

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router";
+import { Home } from "./pages/Home/Home";
+import { MonthStatistics } from "./pages/MonthStatistics/MonthStatistics";
 
 function App() {
   return (
-    <div className="App">asdasda</div>
+    <div className="App">
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/month-statistics" exact component={MonthStatistics} />
+      </Switch>
+    </div>
   );
 }
 
