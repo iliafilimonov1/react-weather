775e4a33151927a2da80f2e0f1eb309ceff58e04
Ilia Filimonov <9991571155i@gmail.com>
2022-06-29 12:33:10 +0300

    fixed homePage, styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import s from "./Home.module.scss";
 import { ThisDay } from "./components/ThisDay/ThisDay";
+import { ThisDayInfo } from "./components/ThisDayInfo/ThisDayInfo";
+import { Days } from "./components/Days/Days";
 
 interface Props {}
 
 export const Home = (props: Props) => {
   return (
-    <section className={s.home}>
-      
+    <>
+      <section className={s.flex}>
         <ThisDay />
-      
-    </section>
+        <ThisDayInfo />
+      </section>
+      <section className={s.flex}>
+        <Days />
+      </section>
+    </>
   );
 };
