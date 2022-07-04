3124ddc3bc6002b052e1e2c2ed05041f272d2f9f
Ilia Filimonov <9991571155i@gmail.com>
2022-07-04 19:25:36 +0300

    added themeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.scss";
+import { ThemeProvider } from "./provider/ThemeProvider";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
