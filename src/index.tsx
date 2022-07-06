15e04059f4908a39b2a2da91c8ecd42dbcbd5502
Ilia Filimonov <9991571155i@gmail.com>
2022-07-06 10:46:24 +0300

    added provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,20 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.scss";
 import { ThemeProvider } from "./provider/ThemeProvider";
+import { Provider } from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
